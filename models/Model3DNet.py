modeify rotation loss and fix bug of display

models/Model3DNet.py
@@ -25,6 +25,7 @@ class Model3DNet(nn.Module):
                  num_samples=6000,
                  num_hidden_feat=256,
                  num_ico_vert=2562,
+                 fps_num=8,
                  device=config["device"]):
         super().__init__()
 
@@ -35,6 +36,7 @@ class Model3DNet(nn.Module):
         # load the 3d model
         self.model_path_list = model_path_list
         self.device = device
+        self.fps_num = fps_num
         self.meshes, self.scale_list, self.center_list, self.fps_list = self.load_models()
         self.scale_list = torch.stack(self.scale_list)[:, None]
         self.center_list = torch.stack(self.center_list)
@@ -127,7 +129,7 @@ class Model3DNet(nn.Module):
         return x.reshape(B, -1, 3), scales, centers
 
     @staticmethod
-    def load_one_model(model_path, device=None):
+    def load_one_model(model_path, fps_num=8, device=None):
         verts, faces, aux = load_obj(model_path)
 
         if device is not None:
@@ -136,7 +138,7 @@ class Model3DNet(nn.Module):
 
         center = verts.mean(0)
 
-        fps_points = Model3DNet.get_fps_points(verts, center)
+        fps_points = Model3DNet.get_fps_points(verts, center, fps_num=fps_num)
         verts = verts - center
         scale = max(verts.abs().max(0)[0])
         verts = verts / scale
@@ -164,7 +166,7 @@ class Model3DNet(nn.Module):
         return fps_3d_points
 
     def load_models(self):
-        verts_faces_list = [Model3DNet.load_one_model(p, self.device) for p in self.model_path_list]
+        verts_faces_list = [Model3DNet.load_one_model(p, self.fps_num, self.device) for p in self.model_path_list]
         verts_list = [vf[0] for vf in verts_faces_list]
         faces_list = [vf[1] for vf in verts_faces_list]
         scale_list = [vf[2] for vf in verts_faces_list]

models/deformable_detr.py
@@ -522,6 +522,7 @@ class SetCriterion(nn.Module):
 
         losses["pose_6dof_class"] = loss_mc
 
+        # ===========================================
         # 2. get 6Dof loss
         # 2.1 get predict 6Dof
         src_pose = outputs["pose_6dof"]
@@ -532,6 +533,7 @@ class SetCriterion(nn.Module):
         src_6dof_t_pose = src_6dof_pose[:, :3]
         src_6dof_r_pose = src_6dof_pose[:, 3:]
 
+        # =============================================
         # 2.2 get target 6Dof pose
         tgt_6dof_t_pose_o = torch.cat([t["T_matrix_c2o"][J] for t, (_, J) in zip(targets, indices)])
         tgt_6dof_r_pose_o = torch.cat([t["R_quaternion_c2o"][J] for t, (_, J) in zip(targets, indices)])
@@ -540,13 +542,16 @@ class SetCriterion(nn.Module):
         tgt_bboxes_2d = torch.cat([t["bboxes_2d"][J] for t, (_, J) in zip(targets, indices)])
         tgt_bboxes_3d_w = torch.cat([t["bboxes_3d_w"][J] for t, (_, J) in zip(targets, indices)])
 
+        # ============================================
         # 2.3 get the model add loss
         losses["pose_6dof_add"] = nn.L1Loss()(src_6dof_t_pose, tgt_6dof_t_pose_o)
 
+        # ============================================
         # TODO: 2.4 get the model l1 loss of 3d bbox
 
         # tgt_bboxes_3d_points = [[tgt_bboxes_3d_w[i][0], tgt_bboxes_3d_w[i][-1]]for i in range(tgt_bboxes_3d_w.shape[0])]
 
+        # ============================================= 
         # 2.5 get the model l1 loss of fps points
         # 2.5.1 get the fps points in object coordinate
         fps_points = targets[0]["fps_points"]
@@ -571,6 +576,11 @@ class SetCriterion(nn.Module):
 
         # 2.5.6 calcuate the l1 loss between target fps points and src fps points
         losses["pose_6dof_fps_points_3d"] = nn.L1Loss()(src_fps_points_camera, tgt_fps_points_camera)
+
+        # ==========================================
+        #  2.6 get the l1 loss of the Rotation
+        losses["pose_6dof_rotation"] = nn.L1Loss()(src_6dof_r_pose, tgt_6dof_r_pose_o)
+
 
         # TODO: 3 get the model bboxes_2d loss
         # losses["pose_6dof_bboxes_2d"] = 0
@@ -729,7 +739,7 @@ def build(args):
     )
 
     if args.poses:
-        model = DETRpose(model, freeze_detr=(args.frozen_weights is not None))
+        model = DETRpose(model, model_fps_num=args.model_fps_num,freeze_detr=(args.frozen_weights is not None))
 
     matcher = build_matcher(args)
     weight_dict = {'loss_ce': args.cls_loss_weight,
@@ -743,6 +753,7 @@ def build(args):
         weight_dict["pose_6dof_class"] = args.model_6dof_class_weight
         weight_dict["pose_6dof_add"] = args.model_6dof_add_weight
         weight_dict["pose_6dof_fps_points_3d"] = args.model_6dof_fps_points_weight
+        weight_dict["pose_6dof_rotation"] = args.model_6dof_rotation_weight
 
     if args.aux_loss:
         aux_weight_dict = {}

models/pose.py
@@ -113,7 +113,7 @@ class PoseHeadSmallLinear(nn.Module):
 
 
 class DETRpose(nn.Module):
-    def __init__(self, detr, freeze_detr=False):
+    def __init__(self, detr, model_fps_num=8, freeze_detr=False):
         super().__init__()
         self.detr = detr
 
@@ -123,7 +123,7 @@ class DETRpose(nn.Module):
 
         hidden_dim, nheads = detr.transformer.d_model, detr.transformer.nhead
 
-        self.model_3d_net = Model3DNet()
+        self.model_3d_net = Model3DNet(fps_num=model_fps_num)
 
         self.bbox_attention = MHAttentionMap(hidden_dim, hidden_dim, nheads, dropout=0.0)
 

util/plot_utils.py
@@ -17,7 +17,7 @@ from pytorch3d.utils import ico_sphere
 from pytorch3d.ops import sample_points_from_meshes
 from pytorch3d.io import load_obj, save_obj
 import json
-from util.utils import get_distance, RQuaternion_2_RMatrix, Rotate_x_axis
+from util.utils import get_distance, RQuaternion_2_RMatrix, Rotate_x_axis, Rotate_y_axis, get_R_w2c
 
 
 
@@ -120,11 +120,12 @@ class PoseResultPlotor(object):
                 
                 if this_epoch == 0 or this_epoch is None:
                     self._plot_bbox2d(sample.permute(1, 2, 0).cpu(), tgt_bboxes_2d, tgt_model_ids, tgt_scores,
-                                        f'{self.tgt_img_save_path}/{save_name}_{num}.png')
+                                        target["orig_size"].cpu(),
+                                        f'{self.tgt_img_save_path}/{save_name}_t{target["img_path"]}.png')
                     if args.poses:
                         # save targets 6dof as annotations for labelImg3d
                         self._save_tgt_6dof_annotation(sample.permute(1, 2, 0).cpu(), tgt_pose_6dof, tgt_model_ids, 
-                                                    f'{self.tgt_annotation_save_path}/{save_name}_{num}.json')
+                                                    f'{self.tgt_annotation_save_path}/{save_name}_t{target["img_path"]}.json')
                         
                         
                 threshold = args.plot_threshold
@@ -138,30 +139,38 @@ class PoseResultPlotor(object):
                         th_pose_6dof = pred_pose_6dof[pred_mask]
 
                         # save pred images
-                        self._plot_bbox2d(sample.permute(1, 2, 0).cpu(), th_bboxes_2d, th_model_ids, th_scores,
-                                            f'{self.pred_img_save_path}/{save_name}_{num}.png')
+                        self._plot_bbox2d(sample.permute(1, 2, 0).cpu(), th_bboxes_2d, th_model_ids, th_scores, 
+                                            target["orig_size"].cpu(),
+                                            f'{self.pred_img_save_path}/{save_name}_t{target["img_path"]}.png')
                         # save pred 6dof as annotations for labelImg3d
-                        self._save_pred_6dof_annotation(sample, th_pose_6dof, th_model_ids, f"{self.pred_annotation_save_path}/{save_name}_{num}.json")
+                        self._save_pred_6dof_annotation(sample, th_pose_6dof, th_model_ids, f'{self.pred_annotation_save_path}/{save_name}_t{target["img_path"]}.json')
                     else:
                         # save pred images
-                        self._plot_bbox2d(sample.permute(1, 2, 0).cpu(), th_bboxes_2d, th_classes, th_scores,
-                                            f'{self.pred_img_save_path}/{save_name}_{num}.png')
+                        self._plot_bbox2d(sample.permute(1, 2, 0).cpu(), th_bboxes_2d, th_classes, th_scores, 
+                                            target["orig_size"].cpu(),
+                                            f'{self.pred_img_save_path}/{save_name}_t{target["img_path"]}.png')
 
                 num = num + 1
         except Exception as e:
             print(e)
 
-    def _plot_bbox2d(self, image, boxes, labels, scores, save_name: str = None):
-        fig = plt.figure(figsize=(16, 10))
+    def _plot_bbox2d(self, image, boxes, labels, scores, img_size, save_name: str = None):
+        fig = plt.figure(figsize=(img_size[0]/100, img_size[1]/100))
         ax = plt.gca()
         ax.axis('off')
-        ima = np.array(image, np.int32)
+        
+        # Remove the white border around the image
+        ax.xaxis.set_major_locator(plt.NullLocator()) 
+        ax.yaxis.set_major_locator(plt.NullLocator()) 
+        plt.subplots_adjust(top=1,bottom=0,left=0,right=1,hspace=0,wspace=0) 
+        plt.margins(0,0)
+
         im = ax.imshow(image)
+
         bbox = self._draw_bbox_2d(ax, boxes, labels, scores)
 
-        ani = animation.ArtistAnimation(fig, [[im] + bbox], interval=200, blit=False)
         if save_name:
-            ani.save(save_name, writer='pillow')
+            fig.savefig(save_name, dpi=100, bbox_inches="tight", pad_inches=0.0)
         plt.close(fig)
 
     def _draw_bbox_2d(self, ax, boxes, labels, scores):
@@ -198,7 +207,7 @@ class PoseResultPlotor(object):
     def _save_pred_6dof_annotation(self, sample, pose_6dof, model_ids, json_save_path):
         save_name = json_save_path.split("/")[-1].split(".")[0]
         data = self.getEmptyJson(save_name)
-        data["model"]["num"] = len(pose_6dof)
+        data["model"]["num"] = len(model_ids)
 
         for i, pose in enumerate(pose_6dof):
             model_save_path = f"models/{KITTI_CLASSES[model_ids[i]]}.obj"
@@ -207,12 +216,11 @@ class PoseResultPlotor(object):
             model_class_id = model_ids.tolist()[i]
 
             pose = pose[model_class_id, :]
-            pose_t = [pose[0], pose[1], -pose[2] + get_distance(config["camera_fov"])]
-            pose_r = np.dot(RQuaternion_2_RMatrix(pose[3:]), Rotate_x_axis(-90)).tolist()
-            pose_r = [pose_r[0], pose_r[2], pose_r[1]]
+            pose_t = [pose[0], -pose[1], -pose[2] + get_distance(config["camera_fov"])]
+            pose_r = np.dot(np.linalg.inv(get_R_w2c()), RQuaternion_2_RMatrix(pose[3:])).tolist()
 
             matrix = np.column_stack([pose_r, pose_t])
-            matrix = np.row_stack([matrix, np.array([0, 0, 0, 1])]).tolist()
+            matrix = np.row_stack([matrix, np.array([0, 0, 0, 1])]).flatten().tolist()
 
             model = load_obj(os.path.join(self.pred_model_path, f"{KITTI_CLASSES[model_ids[i]]}.obj"))[0]
 
@@ -240,12 +248,11 @@ class PoseResultPlotor(object):
             model_class = KITTI_CLASSES[model_ids[i]]
             model_class_id = model_ids.tolist()[i]
 
-            pose_t = [pose[0], pose[1], -pose[2] + get_distance(config["camera_fov"])]
-            pose_r = np.dot(RQuaternion_2_RMatrix(pose[3:]), Rotate_x_axis(-90)).tolist()
-            pose_r = [pose_r[0], pose_r[2], pose_r[1]]
-
+            pose_t = [pose[0], -pose[1], -pose[2] + get_distance(config["camera_fov"])]
+            pose_r = np.dot(np.linalg.inv(get_R_w2c()), RQuaternion_2_RMatrix(pose[3:])).tolist()
+            
             matrix = np.column_stack([pose_r, pose_t])
-            matrix = np.row_stack([matrix, np.array([0, 0, 0, 1])]).tolist()
+            matrix = np.row_stack([matrix, np.array([0, 0, 0, 1])]).flatten().tolist()
 
             model = load_obj(os.path.join(self.pred_model_path, f"{KITTI_CLASSES[model_ids[i]]}.obj"))[0]
 